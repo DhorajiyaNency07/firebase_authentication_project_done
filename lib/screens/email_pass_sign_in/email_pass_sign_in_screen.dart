797d6247cae241afdc561e324586f6deabf19e16
DhorajiyaNency <dhorajiyanency@gmail.com>
2023-03-19 13:16:04 +0530

    19/03/2023

--- lib/screens/email_pass_sign_in/email_pass_sign_in_screen.dart
+++ lib/screens/email_pass_sign_in/email_pass_sign_in_screen.dart
@@ -15,8 +15,8 @@ class _EmailPassSignInScreenState extends State<EmailPassSignInScreen> {
   final ImagePicker _picker = ImagePicker();
   final storage = FirebaseStorage.instance;
 
-  // XFile? image;
-  List<XFile>? images;
+  XFile? image;
+  // List<XFile>? images;
 
   @override
   Widget build(BuildContext context) {
@@ -34,21 +34,6 @@ class _EmailPassSignInScreenState extends State<EmailPassSignInScreen> {
       ),
       body: Column(
         children: [
-          // ElevatedButton(
-          //   onPressed: () {
-          //     pickImage();
-          //   },
-          //   child: Container(
-          //     height: 200,
-          //     color: Colors.greenAccent,
-          //     child: image == null
-          //         ? const SizedBox()
-          //         : Image.file(
-          //             File(image!.path),
-          //             fit: BoxFit.cover,
-          //           ),
-          //   ),
-          // ),
           ElevatedButton(
             onPressed: () {
               pickImage();
@@ -56,20 +41,36 @@ class _EmailPassSignInScreenState extends State<EmailPassSignInScreen> {
             child: Container(
               height: 200,
               color: Colors.greenAccent,
-              child: images == null
+              child: image == null
                   ? const SizedBox()
-                  : ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: images!.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Image.file(
-                          File(images![index].path),
-                          fit: BoxFit.cover,
-                        );
-                      },
+                  : Image.file(
+                      File(image!.path),
+                      fit: BoxFit.cover,
                     ),
             ),
           ),
+          // for multiple image 
+          // ElevatedButton(
+          //   onPressed: () {
+          //     pickImage();
+          //   },
+          //   child: Container(
+          //     height: 200,
+          //     color: Colors.greenAccent,
+          //     child: images == null
+          //         ? const SizedBox()
+          //         : ListView.builder(
+          //             scrollDirection: Axis.horizontal,
+          //             itemCount: images!.length,
+          //             itemBuilder: (BuildContext context, int index) {
+          //               return Image.file(
+          //                 File(images![index].path),
+          //                 fit: BoxFit.cover,
+          //               );
+          //             },
+          //           ),
+          //   ),
+          // ),
 
           ElevatedButton(
             onPressed: () {
@@ -88,57 +89,58 @@ class _EmailPassSignInScreenState extends State<EmailPassSignInScreen> {
     );
   }
 
-  // pickImage() async {
-  //   // Capture a photo
-  //   image = await _picker.pickImage(source: ImageSource.gallery);
-  //   setState(() {});
-  // }
-  //
-  // sendFile() async {
-  //   try {
-  //     final storageRef = FirebaseStorage.instance.ref();
-  //     final janki = storageRef.child("janki.jpg");
-  //     await janki.putFile(File(image!.path));
-  //   } catch (e) {
-  //     debugPrint("Error ------------>>> $e ");
-  //   }
-  // }
-  //
-  // uploadImage() async {
-  //   try {
-  //     File file = File(image!.path);
-  //     storage.ref().child('images/janki.jpg').putFile(file);
-  //   } catch (e) {
-  //     debugPrint(e.toString());
-  //   }
-  // }
-
   pickImage() async {
-    // Capture photos
-    images = await _picker.pickMultiImage();
+    // Capture a photo
+    image = await _picker.pickImage(source: ImageSource.gallery);
     setState(() {});
   }
 
   sendFile() async {
     try {
       final storageRef = FirebaseStorage.instance.ref();
-      for (var i = 0; i < images!.length; i++) {
-        final janki = storageRef.child("images_$i.jpg");
-        await janki.putFile(File(images![i++].path));
-      }
+      final janki = storageRef.child("janki.jpg");
+      await janki.putFile(File(image!.path));
     } catch (e) {
-      debugPrint("Error ------------>>> $e");
+      debugPrint("Error ------------>>> $e ");
     }
   }
 
   uploadImage() async {
     try {
-      for (var i = 0; i < images!.length; i++) {
-        File file = File(images![i++].path);
-        storage.ref().child('images/images_$i.jpg').putFile(file);
-      }
+      File file = File(image!.path);
+      storage.ref().child('images/janki.jpg').putFile(file);
     } catch (e) {
       debugPrint(e.toString());
     }
   }
+
+  // for multiple image
+  // pickImage() async {
+  //   // Capture photos
+  //   images = await _picker.pickMultiImage();
+  //   setState(() {});
+  // }
+  //
+  // sendFile() async {
+  //   try {
+  //     final storageRef = FirebaseStorage.instance.ref();
+  //     for (var i = 0; i < images!.length; i++) {
+  //       final janki = storageRef.child("images_$i.jpg");
+  //       await janki.putFile(File(images![i++].path));
+  //     }
+  //   } catch (e) {
+  //     debugPrint("Error ------------>>> $e");
+  //   }
+  // }
+  //
+  // uploadImage() async {
+  //   try {
+  //     for (var i = 0; i < images!.length; i++) {
+  //       File file = File(images![i++].path);
+  //       storage.ref().child('images/images_$i.jpg').putFile(file);
+  //     }
+  //   } catch (e) {
+  //     debugPrint(e.toString());
+  //   }
+  // }
 }
